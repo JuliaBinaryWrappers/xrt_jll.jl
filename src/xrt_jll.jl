7facee7aff5fb9818d79ff83d2cfcc2413c395a2
jlbuild <juliabuildbot@gmail.com>
2026-07-22 19:42:47 +0000

    xrt_jll build 2.23.0+0

--- src/xrt_jll.jl
+++ src/xrt_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("xrt")
-JLLWrappers.@generate_main_file("xrt", UUID("bb401bc5-fb22-5674-bf0e-333823e8805f"))
+JLLWrappers.@generate_main_file("xrt", Base.UUID("bb401bc5-fb22-5674-bf0e-333823e8805f"))
 end  # module xrt_jll
